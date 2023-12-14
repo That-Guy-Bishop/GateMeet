ecc534517610fe20f42bc64d2376ef3c6492e673
That-Guy-Bishop <136751978+That-Guy-Bishop@users.noreply.github.com>
2023-12-14 21:18:33 +0530

    Add files via upload

---
+++ GateMeet/GateMeet_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GatemeetAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'GateMeet_app'
---
+++ GateMeet/GateMeet_app/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth.views import LoginView
+from .views import dashboard
+from django.contrib.auth.views import LoginView
+from .views import logout_view
+from django.views.generic import RedirectView
+
+urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),  
+    path('credit/', views.credit_view, name='credit'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('logout/', logout_view, name='logout'),
+    path('meeting/', views.GateMeet, name='meeting'),
+    path('join/', views.join_room, name='join'),
+    path('', RedirectView.as_view(url='/credit/')),
+
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
